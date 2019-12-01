ef72d9187d067e11ebc2c2566f38eee381c8be64
Gdraven99 <DavidJYopp@gmail.com>
2019-12-01 16:11:11 -0500

    axios library implemented api key integrated basic picture search funtctionality tested

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID 14378b44e0eec68f824b47f98138c989573677540bd309761975be102321936a'
+      }
+    })
   }
 
   render() {
